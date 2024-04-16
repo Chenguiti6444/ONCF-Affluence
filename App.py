975a389b1f779bd91b31815ad298e1b01330f228
ChenguitiElMehdi <118683892+Chenguiti6444@users.noreply.github.com>
2024-04-16 15:27:51 +0000

    Changed App

--- App.py
+++ App.py
@@ -8,8 +8,16 @@ import numpy as np
 from PIL import Image
 from sklearn.preprocessing import StandardScaler
 
+# Configuration de la page
+st.set_page_config(layout="wide")
 # Chargez les données
 data = pd.read_csv('Aggregation des N° de Trains par OD+Gamme+Heure.csv')
+
+# Séparation des colonnes 'Origine' et 'Destination'
+data[['Origine', 'Destination']] = data['OD'].str.split(' - ', expand=True)
+origines = sorted([origine.strip() for origine in data['Origine'].unique()])
+destinations = sorted([destination.strip() for destination in data['Destination'].unique()])
+
 
 # Chargement du modèle et des préprocesseurs
 @st.cache_resource
@@ -42,7 +50,13 @@ def preprocess_inputs(heure_choice, gamme_choice, num_train_choice, selected_dat
     inputs = scaler.transform(inputs)
     return inputs
 
+# Fonction pour mettre à jour les destinations basées sur l'origine sélectionnée
+def updateDestination(origine):
+    filtered_data = data[data['Origine'] == origine]
+    unique_destinations = sorted(filtered_data['Destination'].unique())
+    return unique_destinations
 
+# Fonction pour mettre à jour les heures basées sur l'Origine et la Destination
 def update_heures(od_choice, selected_date):
     # Filtrez les données pour l'OD sélectionné
     filtered_data = data[data['OD'] == od_choice]
@@ -58,19 +72,16 @@ def update_heures(od_choice, selected_date):
     heures = [str(heure) for heure in heures]
     return heures
 
-
+# Fonction pour mettre à jour les gammes basées sur l'Origine, la Destination et l'Heure
 def update_gammes(od_choice, heure_choice):
-    if heure_choice is None:
-        return []  # Return an empty list if heure_choice is None
     # Filtrez les données pour l'OD et l'heure sélectionnés
     filtered_data = data[(data['OD'] == od_choice) & (data['Heure'] == int(heure_choice))]
     # Extrayez les valeurs uniques de gamme, triez-les et convertissez-les en chaînes de caractères
     gammes = sorted(filtered_data['Gamme'].dropna().unique().astype(str).tolist())
     return gammes
 
+# Fonction pour mettre à jour les numéros de train basés sur l'Origine, la Destination, l'Heure et la Gamme
 def update_num_train(od_choice, heure_choice, gamme_choice):
-    if heure_choice is None or gamme_choice is None:
-        return []  # Return an empty list if heure_choice or gamme_choice is None
     # Filtrez les données pour l'OD, l'heure et la gamme sélectionnés
     filtered_data = data[(data['OD'] == od_choice) & 
                          (data['Heure'] == int(heure_choice)) & 
@@ -78,48 +89,6 @@ def update_num_train(od_choice, heure_choice, gamme_choice):
     # Extrayez les numéros de train uniques, séparez-les par virgule, aplatissez la liste et triez
     num_trains = sorted(set([train.strip() for sublist in filtered_data['N° de train'].dropna().str.split(',') for train in sublist]))
     return num_trains
-
-
-st.set_page_config(layout="wide")
-
-# Chargement du logo et affichage en entête de page
-logo = Image.open('Images/Logo.png')
-st.image(logo, width=200)
-
-# Utilisation de st.columns pour diviser la page en deux colonnes
-col1, col2 = st.columns(2)
-
-with col1:
-    # Sélection de la date
-    selected_date = st.date_input("Date de voyage", min_value=datetime.today(), value=None,help="Veuillez choisir une date")
-
-    # Si une date est choisie, affichez la sélection d'OD
-    if selected_date:
-        od_choice = st.selectbox("Origine-Destination", sorted(data['OD'].unique().tolist()), index=None,placeholder="Veuillez choisir l'OD")
-    else:
-        od_choice = None
-
-    # Si un OD est choisi, affichez la sélection d'heure
-    if od_choice:
-        heures = update_heures(od_choice, selected_date)
-        heure_choice = st.selectbox("Heure de voyage", heures, index=None,placeholder="Veuillez choisir l'heure")
-    else:
-        heure_choice = None
-
-with col2:
-    # Si une heure est choisie, affichez la sélection de gamme
-    if heure_choice:
-        gammes = update_gammes(od_choice, heure_choice)
-        gamme_choice = st.selectbox("Gamme", gammes, index=None,placeholder="Veuillez choisir la gamme")
-    else:
-        gamme_choice = None
-
-    # Si une gamme est choisie, affichez la sélection du numéro de train
-    if gamme_choice:
-        num_trains = update_num_train(od_choice, heure_choice, gamme_choice)
-        num_train_choice = st.selectbox("Numéro de train", num_trains, index=0)
-    else:
-        num_train_choice = None
 
 # Initialisation des préprocesseurs et du modèle
 model_path = 'RegDNN_model.tf'
@@ -131,7 +100,7 @@ model, label_encoders, scaler = load_model_and_preprocessors(model_path, label_e
 def calculate_all_predictions(date, od_choice,heure_choice):
     # Convertir l'heure choisie en integer pour la comparaison
     selected_heure_int = int(heure_choice)
-    heures = [heure for heure in update_heures(od_choice, date) if int(heure) > selected_heure_int]
+    heures = [heure for heure in update_heures(od_choice,heure_choice) if int(heure) > selected_heure_int]
     all_combinations = []
     results = []
 
@@ -163,45 +132,75 @@ def display_results(results):
     sorted_results = results_df.sort_values(by='Prédiction').head(5)
     st.table(sorted_results)
 
-# Bouton de prédiction, actif seulement si tous les champs sont remplis
-with col2:
-    if selected_date and od_choice and heure_choice and gamme_choice and num_train_choice:
-        if st.button('Prédire l\'affluence'):
-            inputs_preprocessed = preprocess_inputs(heure_choice, gamme_choice, num_train_choice, selected_date, label_encoders, scaler)
-            prediction = model.predict(inputs_preprocessed)
-            prediction_value = prediction[0]
+logo = Image.open('Images/Logo.png')
+st.image(logo, width=200)
 
-            if prediction_value < 0.10:
-                st.image("Images/Affluence Faible.png", width=200)
-            elif prediction_value < 0.22:
-                st.image("Images/Affluence Moyenne.png", width=200)
-            else:
-                st.image("Images/Affluence Forte.png", width=200)
-            if selected_date and od_choice and num_train_choice and prediction_value>0.10:
-                results = calculate_all_predictions(selected_date, od_choice,heure_choice)
+# Utilisation de st.columns pour diviser la page en deux colonnes
+col1, col2 = st.columns(2)
 
-                # Ensuite, comparez les prédictions et calculez le pourcentage de différence
-                results_with_comparison = []
+# Sélections de date, heure, gamme, et numéro de train
+with col1:  
+    selected_date = st.date_input("Date de voyage", min_value=datetime.today(),value=None,help="Veuillez choisir une date")
+    origines = sorted(data['Origine'].unique())
+    col3, col4 = st.columns(2)
+    with col3:
+        if selected_date: 
+            origine_choice = st.selectbox("Gare de Départ", origines, index=None,placeholder="Veuillez choisir une gare")
+            if origine_choice:
+                with col4:
+                    destination_choice = st.selectbox("Gare d'arrivée", updateDestination(origine_choice), index=None,placeholder="Veuillez choisir une gare")
+                    if destination_choice:
+                        od_choice = f"{origine_choice} - {destination_choice}"
+                        with col2:
+                            # Utilisez la fonction update_heures pour obtenir la liste des heures
+                            heures_list = update_heures(od_choice, selected_date)
+                            # Vérifiez si la liste des heures est vide avant d'afficher le selectbox
+                            if heures_list:
+                                heure_choice = st.selectbox("Heure de voyage", heures_list, index=None, placeholder="Veuillez choisir l'heure")
+                                if heure_choice:
+                                    gamme_choice = st.selectbox("Gamme", update_gammes(od_choice, heure_choice), index=None, placeholder="Veuillez choisir la gamme")
+                                    if gamme_choice:
+                                        num_train_choice = st.selectbox("Numéro de train", update_num_train(od_choice, heure_choice, gamme_choice), index=0)
+                                        # Bouton de prédiction, actif seulement si tous les champs sont remplis
 
-                for result in results:
-                    # Extraire le scalaire de la prédiction avant de faire le calcul
-                    prediction_result = result['Prédiction']
-                    difference_percentage = ((prediction_value - prediction_result) / prediction_value) * 100
-                    # Ici, nous convertissons le pourcentage en scalaire pour éviter les erreurs de formatage
-                    difference_percentage_value = round(difference_percentage.item())
-                    if difference_percentage_value>0:
-                        result["Différence d'Affluence"] = f"{difference_percentage_value}% moins chargé que le train choisi"
-                        results_with_comparison.append(result)
+                                        if selected_date and od_choice and heure_choice and gamme_choice and num_train_choice:
+                                            if st.button('Prédire l\'affluence'):
+                                                inputs_preprocessed = preprocess_inputs(heure_choice, gamme_choice, num_train_choice, selected_date, label_encoders, scaler)
+                                                prediction = model.predict(inputs_preprocessed)
+                                                prediction_value = prediction[0]
 
-                # Triez les résultats pour obtenir les prédictions les plus faibles par rapport à la référence
-                results_df = pd.DataFrame(results_with_comparison)
-                sorted_results = results_df.sort_values(by='Prédiction').head(5)
-                # Réinitialiser l'index du DataFrame et supprimer l'ancien index
-                sorted_results = sorted_results.reset_index(drop=True)
+                                                if prediction_value < 0.10:
+                                                    st.image("Images/Affluence Faible.png", width=200)
+                                                elif prediction_value < 0.23:
+                                                    st.image("Images/Affluence Moyenne.png", width=200)
+                                                else:
+                                                    st.image("Images/Affluence Forte.png", width=200)
+                                                if selected_date and od_choice and num_train_choice and prediction_value>0.10:
+                                                    results = calculate_all_predictions(selected_date, od_choice,heure_choice)
 
-                with col1:
-                    st.subheader('Suggestions de Trains Moins Chargés')
-                    # Affichez les résultats dans un tableau
-                    st.dataframe(sorted_results[['Heure', 'Gamme', 'Numéro de Train', "Différence d'Affluence"]],hide_index=True)
+                                                    # Ensuite, comparez les prédictions et calculez le pourcentage de différence
+                                                    results_with_comparison = []
 
+                                                    for result in results:
+                                                        # Extraire le scalaire de la prédiction avant de faire le calcul
+                                                        prediction_result = result['Prédiction']
+                                                        difference_percentage = ((prediction_value - prediction_result) / prediction_value) * 100
+                                                        # Ici, nous convertissons le pourcentage en scalaire pour éviter les erreurs de formatage
+                                                        difference_percentage_value = round(difference_percentage.item())
+                                                        if difference_percentage_value>0:
+                                                            result["Différence d'Affluence"] = f"{difference_percentage_value}% moins chargé que le train choisi"
+                                                            results_with_comparison.append(result)
+
+                                                    # Triez les résultats pour obtenir les prédictions les plus faibles par rapport à la référence
+                                                    results_df = pd.DataFrame(results_with_comparison)
+                                                    sorted_results = results_df.sort_values(by='Prédiction').head(5)
+                                                    # Réinitialiser l'index du DataFrame et supprimer l'ancien index
+                                                    sorted_results = sorted_results.reset_index(drop=True)
+
+                                                    with col1:
+                                                        st.subheader('Suggestions de Trains Moins Chargés')
+                                                        # Affichez les résultats dans un tableau
+                                                        st.dataframe(sorted_results[['Heure', 'Gamme', 'Numéro de Train', "Différence d'Affluence"]],hide_index=True)
+                            else:
+                                st.warning("Aucun train n'est disponible aujourd'hui pour le trajet sélectionné.")
 
